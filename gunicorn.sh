e71aaeddb7210ae8399570a4f7e6e276e1d12910
root@localhost <root@localhost>
2022-03-06 09:56:12 +0100

    SSL

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-exec gunicorn --chdir app main:app -w 2 --threads 1 -b 0.0.0.0:5000
+exec gunicorn --chdir app  -w 2 --threads 1 -b 0.0.0.0:5000 --certfile=/certs/www.europehelp.info.crt --keyfile=/certs/www.europehelp.info.key main:app
