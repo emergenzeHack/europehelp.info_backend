df9350e49e5aeed15d0aec5eedb96eb4c6f8fbd7
root@localhost <root@localhost>
2022-03-14 10:28:31 +0100

    Fix changed name

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-exec gunicorn --chdir app  -w 2 --threads 1 -b 0.0.0.0:5000 --certfile=/certs/www.europehelp.info.crt --keyfile=/certs/www.europehelp.info.key main:app
+exec gunicorn --chdir app  -w 2 --threads 1 -b 0.0.0.0:5000 --certfile=/certs/ukrainehelp.emergenzehack.info.crt --keyfile=/certs/ukrainehelp.emergenzehack.info.key main:app
