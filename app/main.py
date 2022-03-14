b693a45595e1e48d5721d15f9f8574c409345550
Antonio Vivace <antonio.vivace@cern.ch>
2022-03-14 22:08:46 +0100

    Reformatting

--- app/main.py
+++ app/main.py
@@ -1,21 +1,22 @@
+import json
+import logging
+
+import credentials
+import requests
+import yaml
 from flask import Flask, request
 from flask_cors import CORS
-import requests
-import yaml, json
-import credentials
-import re
-import logging
 from geopy.geocoders import Nominatim
-import logging
+
 geolocator = Nominatim(user_agent="europehelp.info")
 
-with open('italy_geo.json') as f:
+with open("italy_geo.json") as f:
     italy_geo = json.load(f)
 
-with open('comuni.json') as f:
+with open("comuni.json") as f:
     comuni = json.load(f)
 
-comuni = sorted(comuni, key=lambda k: k['popolazione'], reverse=True)
+comuni = sorted(comuni, key=lambda k: k["popolazione"], reverse=True)
 
 app = Flask(__name__)
 
@@ -28,36 +29,38 @@ USERNAME = credentials.user
 PASSWORD = credentials.password
 
 # The repository to add this issue to
-REPO_OWNER = 'emergenzeHack'
-REPO_NAME = 'europehelp.info_segnalazioni'
+REPO_OWNER = "emergenzeHack"
+REPO_NAME = "europehelp.info_segnalazioni"
 
-repo_names = {
-    'en': 'europehelp.info_segnalazioni'
-}
+repo_names = {"en": "europehelp.info_segnalazioni"}
 
-@app.route('/')
+
+@app.route("/")
 def paynoattention():
-    app.logger.info ("404")
-    return 'Pay no attention to that man behind the curtain!', 404
+    app.logger.info("404")
+    return "Pay no attention to that man behind the curtain!", 404
 
-@app.route('/webhook')
+
+@app.route("/webhook")
 def webhook():
     app.logger.info("request headers {}".format(request.headers))
     app.logger.info("request JSON {}".format(request.json))
     return "OK", 200
 
-@app.route('/report', methods=['POST'])
+
+@app.route("/report", methods=["POST"])
 def report():
     app.logger.info("request headers {}".format(request.headers))
     app.logger.info("request JSON {}".format(request.json))
     process_report(request.json, request.headers)
     return "OK", 200
 
-def process_report(request, headers_pre, additional_labels=[],issue_title=None):
-    payload=request['data']
+
+def process_report(request, headers_pre, additional_labels=[], issue_title=None):
+    payload = request["data"]
     for k in payload.keys():
         try:
-            payload[k]=payload[k].strip()
+            payload[k] = payload[k].strip()
         except Exception as e:
             print(e)
             pass
@@ -72,51 +75,51 @@ def process_report(request, headers_pre, additional_labels=[],issue_title=None):
     session.auth = (USERNAME, PASSWORD)
 
     labels = []
-    #payload = request.json
+    # payload = request.json
 
     # Rimuovi nome gruppo dal nome delle chiavi
     # e.g. datibancari/IBAN -> datibancari
     for key_name in list(payload):
         if "datibancari/" in key_name:
-            new_key_name = key_name.replace("datibancari/","")
+            new_key_name = key_name.replace("datibancari/", "")
             payload[new_key_name] = payload[key_name]
             payload.pop(key_name)
 
-    if 'label' in list(payload):
-        label = payload['label']
+    if "label" in list(payload):
+        label = payload["label"]
     else:
         label = "form_raw"
 
     # If country is not specified, IT is default
-    if 'country' in list(headers):
-        country = headers['country']
+    if "country" in list(headers):
+        country = headers["country"]
     else:
-        country = 'en'
+        country = "en"
 
     location = False
     location_geo = False
     comment_body = None
-    
-    if issue_title == None:
+
+    if issue_title is None:
         # Prepare the issue title
         meaningful_fields = {
-            'it' : ["Titolo", "Cosa", "Testo", "Descrizione"],
-            'pt' : ["Nome", "Finalidade"],
-            'gr' : [],
-            'en' : ["title","what","description"],
+            "it": ["Titolo", "Cosa", "Testo", "Descrizione"],
+            "pt": ["Nome", "Finalidade"],
+            "gr": [],
+            "en": ["title", "what", "description"],
         }
 
         for field in meaningful_fields[country]:
             if field in list(payload):
                 if len(payload[field]) > 0:
-                    issue_title=payload[field][0:100]
+                    issue_title = payload[field][0:100]
                     break
 
         if "Chi" in list(payload) and label == "Raccolte fondi":
             issue_title = "Raccolta fondi %s" % (payload["Chi"])
 
         if not issue_title:
-            issue_title=label
+            issue_title = label
 
     if country == "it":
         # Assegna le label in base alle selezioni sul form "Iniziative"
@@ -136,14 +139,20 @@ def process_report(request, headers_pre, additional_labels=[],issue_title=None):
                 elif payload["Natura"] == "sostegno-lavor":
                     label = "Sostegno lavoro e imprese"
 
-        # Se trovi riferimenti a psicologi o psicoterapeuti, 
+        # Se trovi riferimenti a psicologi o psicoterapeuti,
         #  aggiungi la label "Supporto Psicologico"
         if "Titolo" in list(payload):
-            if "psicolog" in payload["Titolo"].lower() or "psicoter" in payload["Titolo"].lower():
-                    labels.append("Supporto Psicologico")
+            if (
+                "psicolog" in payload["Titolo"].lower()
+                or "psicoter" in payload["Titolo"].lower()
+            ):
+                labels.append("Supporto Psicologico")
             else:
                 if "Descrizione" in list(payload):
-                    if "psicolog" in payload["Descrizione"].lower() or "psicoter" in payload["Descrizione"].lower():
+                    if (
+                        "psicolog" in payload["Descrizione"].lower()
+                        or "psicoter" in payload["Descrizione"].lower()
+                    ):
                         labels.append("Supporto Psicologico")
 
         # Suggerisci una posizione
@@ -160,7 +169,7 @@ def process_report(request, headers_pre, additional_labels=[],issue_title=None):
             elif "Descrizione" in list(payload) and not location:
                 location = extract_location(payload["Descrizione"])
 
-        if "location" in payload and "Posizione" not in  payload:
+        if "location" in payload and "Posizione" not in payload:
             location_geo = None
             tries = 0
             while location_geo is None:
@@ -170,40 +179,44 @@ def process_report(request, headers_pre, additional_labels=[],issue_title=None):
                         print("Getting coordinates. (%s)" % (tries))
                         location_input = payload["location"]
                         location_geo = geolocator.geocode(payload["location"])
-                    except:
+                    except Exception:
                         pass
                 else:
                     location_geo = 0
-            if location_geo is not 0:
-                payload["Posizione"] = str(location_geo.latitude) +" "+str(location_geo.longitude)
+            if location_geo != 0:
+                payload["Posizione"] = (
+                    str(location_geo.latitude) + " " + str(location_geo.longitude)
+                )
                 payload.pop("location")
 
         if location_geo:
             coords = payload["Posizione"].split(" ")
-            comment_message = "Sembra che questa segnalazione non sia geolocalizzata. Ho automaticamente aggiunto %s (%s) come coordinate. Per favore, controlla <a href='https://nominatim.openstreetmap.org/search.php?q=%s+%s&polygon_geojson=1&viewbox='>qui</a> se sono corrette. In caso positivo, rimuovi pure la label 'Posizione da verificare' da questa Issue, altrimenti, procedi a correggere o rimuovere la posizione come spiegato <a href='https://github.com/emergenzeHack/covid19italia/wiki/Lavorare-sulle-segnalazioni#aggiungere-geolocalizzazione'>qui</a>." % (payload["Posizione"], location_input, coords[0], coords[1])
-            comment_body = {
-                "body": comment_message
-            }
+            comment_message = (
+                "Sembra che questa segnalazione non sia geolocalizzata. Ho automaticamente aggiunto %s (%s) come coordinate. Per favore, controlla <a href='https://nominatim.openstreetmap.org/search.php?q=%s+%s&polygon_geojson=1&viewbox='>qui</a> se sono corrette. In caso positivo, rimuovi pure la label 'Posizione da verificare' da questa Issue, altrimenti, procedi a correggere o rimuovere la posizione come spiegato <a href='https://github.com/emergenzeHack/covid19italia/wiki/Lavorare-sulle-segnalazioni#aggiungere-geolocalizzazione'>qui</a>."
+                % (payload["Posizione"], location_input, coords[0], coords[1])
+            )
+            comment_body = {"body": comment_message}
             labels.append("Posizione da verificare")
 
         # Commenta con il suggerimento
         if location:
-            #payload["Indirizzo"] = location[0]
+            # payload["Indirizzo"] = location[0]
             coords = location[0].split(" ")
-            comment_message = "Sembra che questa segnalazione non sia geolocalizzata. Ho automaticamente aggiunto %s (%s) come coordinate. Per favore, controlla <a href='https://nominatim.openstreetmap.org/search.php?q=%s+%s&polygon_geojson=1&viewbox='>qui</a> se sono corrette. In caso positivo, rimuovi pure la label 'Posizione da verificare' da questa Issue, altrimenti, procedi a correggere o rimuovere la posizione come spiegato <a href='https://github.com/emergenzeHack/covid19italia/wiki/Lavorare-sulle-segnalazioni#aggiungere-geolocalizzazione'>qui</a>." % (location[0], location[1], coords[0], coords[1])
-            comment_body = {
-                "body": comment_message
-            }
+            comment_message = (
+                "Sembra che questa segnalazione non sia geolocalizzata. Ho automaticamente aggiunto %s (%s) come coordinate. Per favore, controlla <a href='https://nominatim.openstreetmap.org/search.php?q=%s+%s&polygon_geojson=1&viewbox='>qui</a> se sono corrette. In caso positivo, rimuovi pure la label 'Posizione da verificare' da questa Issue, altrimenti, procedi a correggere o rimuovere la posizione come spiegato <a href='https://github.com/emergenzeHack/covid19italia/wiki/Lavorare-sulle-segnalazioni#aggiungere-geolocalizzazione'>qui</a>."
+                % (location[0], location[1], coords[0], coords[1])
+            )
+            comment_body = {"body": comment_message}
             labels.append("Posizione da verificare")
 
     if "location" in payload:
-        location=payload["location"]
+        location = payload["location"]
         address = location["address"]
-        if "lat" in  address:
+        if "lat" in address:
             lat = address["lat"]
             if "lon" in address:
-                lon=address["lon"]
-                payload["Posizione"]=str(lat)+" "+str(lon)
+                lon = address["lon"]
+                payload["Posizione"] = str(lat) + " " + str(lon)
 
     positionFound = None
     positionLabels = ["Posizione", "location", "Indirizzo"]
@@ -220,7 +233,7 @@ def process_report(request, headers_pre, additional_labels=[],issue_title=None):
             labels.append("Missing position")
 
     # Aggiungi sempre la label "form" per le issue provenienti da questo script
-    #labels.append("form")
+    # labels.append("form")
     # Aggiungi le label preparate
     labels.append(label)
 
@@ -233,32 +246,47 @@ def process_report(request, headers_pre, additional_labels=[],issue_title=None):
     blacklist = ["promozioniltaliane.com", "vuedc.info"]
 
     # FIXME this has to be restored in some other way
-    #for word in blacklist:
+    # for word in blacklist:
     #    for field in list(payload):
     #        if word in payload[field]:
     #            labels.append("spam")
 
     # Prepara il payload in YAML
-    yaml_payload = "<pre><yamldata>\n"+yaml.dump(stripped_payload, allow_unicode=True,sort_keys=False)+"</yamldata></pre>"
+    yaml_payload = (
+        "<pre><yamldata>\n"
+        + yaml.dump(stripped_payload, allow_unicode=True, sort_keys=False)
+        + "</yamldata></pre>"
+    )
 
     # Apri issue su GitHub
-    comment_url = open_github_issue(session, title=issue_title, body=yaml_payload, labels=labels, country=country)
-    
+    comment_url = open_github_issue(
+        session, title=issue_title, body=yaml_payload, labels=labels, country=country
+    )
+
     if comment_body:
         add_comment(session, url=comment_url, body=comment_body)
 
     return
 
+
 def strip_meta(payload):
-    excludelist = ["end", "start", "formhub/uuid", "meta/instanceID", "meta/deprecatedID", "Informativa"]
+    excludelist = [
+        "end",
+        "start",
+        "formhub/uuid",
+        "meta/instanceID",
+        "meta/deprecatedID",
+        "Informativa",
+    ]
     # Rimuovi tutti i campi meta che iniziano con _
     for k in list(payload):
-        if k.startswith('_'):
+        if k.startswith("_"):
             payload.pop(k)
         # Rimuovi anche le chiavi specificate nella lista
         elif k in excludelist:
             payload.pop(k)
     return payload
+
 
 def extract_location(text):
     for comune in comuni[0:150]:
@@ -272,44 +300,54 @@ def extract_location(text):
 
     return False
 
+
 def add_comment(session, url, body):
     r = session.post(url, json.dumps(body))
     if r.status_code == 201:
-        print('Successfully created Comment on Issue')
+        print("Successfully created Comment on Issue")
 
     else:
-        print('Could not create Comment', title)
-        print('Response:', r.content)
+        print("Could not create Comment", title)
+        print("Response:", r.content)
 
-def open_github_issue(session, title, body=None, assignee=None, milestone=None, labels=[], country='it'):
-    '''Create an issue on github.com using the given parameters.'''
+
+def open_github_issue(
+    session, title, body=None, assignee=None, milestone=None, labels=[], country="it"
+):
+    """Create an issue on github.com using the given parameters."""
 
     # Create our issue
-    issue = {'title': title,
-             'body': body,
-             'assignee': assignee,
-             'milestone': milestone,
-             'labels': labels}
-    
+    issue = {
+        "title": title,
+        "body": body,
+        "assignee": assignee,
+        "milestone": milestone,
+        "labels": labels,
+    }
+
     # Our url to create issues via POST
-    url = 'https://api.github.com/repos/%s/%s/issues' % (REPO_OWNER, repo_names[country])
-    
+    url = "https://api.github.com/repos/%s/%s/issues" % (
+        REPO_OWNER,
+        repo_names[country],
+    )
+
     # Add the issue to our repository
     r = session.post(url, json.dumps(issue))
-    
+
     if r.status_code == 201:
-        print('Successfully created Issue', title)
+        print("Successfully created Issue", title)
         response = r.json()
         return response["comments_url"]
 
     else:
-        print('Could not create Issue', title)
-        print('Response:', r.content)
+        print("Could not create Issue", title)
+        print("Response:", r.content)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
 else:
-    gunicorn_logger = logging.getLogger('gunicorn.error')
+    gunicorn_logger = logging.getLogger("gunicorn.error")
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
-    app.logger.info ("Using gunicorn logger")
+    app.logger.info("Using gunicorn logger")
