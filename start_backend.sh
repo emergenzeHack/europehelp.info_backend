83614339aa87e7d8f8a327b04478ca339077aceb
root@localhost <root@localhost>
2022-03-14 10:28:15 +0100

    Fix changed name

--- start_backend.sh
+++ start_backend.sh
@@ -1,4 +1,4 @@
-docker build . --tag europehelp_backend
-docker stop europehelp_backend; docker rm europehelp_backend
+docker build . --tag ukrainehelp_backend
+docker stop ukrainehelp_backend; docker rm ukrainehelp_backend
 
-docker run -d -p 5000:5000 --name "europehelp_backend" europehelp_backend
+docker run -d -p 5000:5000 --name "ukrainehelp_backend" -v /root/docker/nginx-proxy/certsHTTPS/:/certs:ro ukrainehelp_backend
